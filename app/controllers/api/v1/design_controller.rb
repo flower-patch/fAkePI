2b5f8235c77b8b4876ed28117a5ac08f67992243
C Haden <chaden@spoonflower.com>
2015-03-25 14:01:35 -0400

    add CORS headers to API

--- app/controllers/api/v1/design_controller.rb
+++ app/controllers/api/v1/design_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::DesignController < ApplicationController
 
+  before_action :add_cors_headers
   # default: return 10 results
   def search
     default_width = 150
@@ -25,6 +26,14 @@ class Api::V1::DesignController < ApplicationController
     render json: { results: [ {results: result_array} ] }
 
   end
+
+  def add_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
   # design/search?availability=for_sale&substrate=fabric&color1=#{color1}
 
   # http://api.v1.spoonflower.com/design/search?q=#{search_term}
